c88440e41c60fae0b49b0905b7a7aa9bdba0dd44
Christian <christian@santins.com.br>
2023-09-04 02:46:03 -0300

    add tailwind

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,9 @@
+<script setup lang="ts">
+import WorkInProgress from '../components/svgs/WorkInProgress.vue'
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="flex flex-col justify-center items-center gap-12 mt-20">
+    <h1>Work in progres...</h1>
+    <WorkInProgress class="h-96 w-96"/>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import WelcomeCats from '../components/svgs/WelcomeCats.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="flex flex-col justify-center items-center gap-12 mt-20">
+      <WelcomeCats class="h-96 w-96" />
+    </div>
   </main>
 </template>
