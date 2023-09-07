4ccb40ad3166ffa1cbad384d60dbd1d4e5f91eb4
Christian <christian@santins.com.br>
2023-09-07 10:09:16 -0300

    teste

--- src/components/PuzzleGame.vue
+++ src/components/PuzzleGame.vue
@@ -156,7 +156,7 @@ async function downloadImage() {
 
                 <!-- <img v-if="cell" :src="`/blog/src/components/images/${cell}.jpg`" alt=""
                     :class="{ 'fade-in-after-solved': isSolved && emptyCell.row === board.indexOf(row) && emptyCell.col === row.indexOf(cell) }"> -->
-                <img v-if="cell" :src="`../components/images/${cell}.jpg`" alt=""
+                <img v-if="cell" :src="`../CyberAPOKA/blog/blob/main/src/components/images/${cell}.jpg`" alt=""
                     :class="{ 'fade-in-after-solved': isSolved && emptyCell.row === board.indexOf(row) && emptyCell.col === row.indexOf(cell) }">
 
                 <!-- <span class="absolute text-red-600 text-5xl">{{ cell }}</span> -->
