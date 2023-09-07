9e2d3cb22ecdcb14fa010aafdd442ac21824ae58
Christian <christian@santins.com.br>
2023-09-07 10:23:44 -0300

    qwdqwd

--- src/components/PuzzleGame.vue
+++ src/components/PuzzleGame.vue
@@ -154,12 +154,11 @@ async function downloadImage() {
                 :class="{ 'border-primary border': !isSolved, empty: !cell }"
                 @click="move(board.indexOf(row), row.indexOf(cell))">
 
-                <!-- <img v-if="cell" :src="`/blog/src/components/images/${cell}.jpg`" alt=""
+                <img v-if="cell" :src="`/blog/src/components/images/${cell}.jpg`" alt=""
+                    :class="{ 'fade-in-after-solved': isSolved && emptyCell.row === board.indexOf(row) && emptyCell.col === row.indexOf(cell) }">
+                <!-- <img v-if="cell" :src="`../components/images/${cell}.jpg`" alt=""
                     :class="{ 'fade-in-after-solved': isSolved && emptyCell.row === board.indexOf(row) && emptyCell.col === row.indexOf(cell) }"> -->
-                <img v-if="cell" :src="`../components/images/${cell}.jpg`" alt=""
-                    :class="{ 'fade-in-after-solved': isSolved && emptyCell.row === board.indexOf(row) && emptyCell.col === row.indexOf(cell) }">
-                <img v-if="cell" :src="`../assets/images/${cell}.jpg`" alt=""
-                    :class="{ 'fade-in-after-solved': isSolved && emptyCell.row === board.indexOf(row) && emptyCell.col === row.indexOf(cell) }">
+
                 <!-- <span class="absolute text-red-600 text-5xl">{{ cell }}</span> -->
             </div>
         </div>
